3355c456c461caf452e47e6807b36ab2853297d9
jotacarvalh0 <josecarvalho11235@gmail.com>
2025-01-07 12:00:01 -0300

    FEAT: Introduction to data validation.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,9 +14,19 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-        echo $request->input('text_username');
-        echo '<br>';
-        echo $request->input('text_password');
+        // for validation
+        $request->validate(
+            [
+                'text_username' => 'required',
+                'text_password' => 'required'
+            ]
+        );
+
+        // get user input
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        echo 'OK!';
     }
 
     public function logout()
