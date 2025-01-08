be87c3b7f9c535678cc0f08c0d3a9d6c28bc5c49
jotacarvalh0 <josecarvalho11235@gmail.com>
2025-01-08 12:46:16 -0300

    REFACTOR: Linking buttons with routes.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::middleware([CheckIsNotLogged::class])->group(function(){
 
 // app routes - user logged
 Route::middleware([CheckIsLogged::class])->group(function(){
-    Route::get('/logout', [AuthController::class, 'logout']);
-    Route::get('/', [MainController::class, 'index']);
-    Route::get('/newNote', [MainController::class, 'newNote']);
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/', [MainController::class, 'index'])->name('home');
+    Route::get('/newNote', [MainController::class, 'newNote'])->name('new');
 });
