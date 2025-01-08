e0350031e9dd56fdd40c68785d7c28a5510a541a
jotacarvalh0 <josecarvalho11235@gmail.com>
2025-01-08 11:46:45 -0300

    REFACTOR: Basic user authentication.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,15 +38,38 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        // get all the users from the database
-        // $users = User::all()->toArray();
+        // Check if user exists
+        $user = User::where('username', $username)
+                        ->where('deleted_at', NULL)
+                        ->first();
+        if(!$user){
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with('loginError', 'Username ou password incorreto.');
+        }
 
-        // as an object instance of the model`s class
-        $userModel =  new User();
-        $users = $userModel->all()->toArray();
+        // chack if password is correct
+        if(!password_verify($password, $user->password)){
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with('loginError', 'Username ou password incorreto.');
+        }
 
-        echo '<pre>';
-        print_r($users);
+        // update last login
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
+
+        // login user
+        session([
+            'user' => [
+                'id' => $user->id,
+                'username' => $user->username
+            ]
+        ]);
+
+        echo 'LOGIM COM SUCESSO!';
     }
 
     public function logout()
