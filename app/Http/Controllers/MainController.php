795d1da0670288f0e4420fc8421b57b86dfa0df0
jotacarvalh0 <josecarvalho11235@gmail.com>
2025-01-09 13:09:00 -0300

    REFACTOR: Form validation and creation of new note.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use App\Models\User;
 use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
@@ -28,7 +29,37 @@ class MainController extends Controller
 
     public function newNoteSubmit(Request $request)
     {
-        echo "I'm creating a new note.";
+        // validate request
+        $request->validate(
+            // rules
+            [
+                'text_title' => 'required | min:3 | max:200',
+                'text_note' => 'required | min:3 | max:3000'
+            ],
+            //error messages
+            [
+                'text_title.required' => 'Title is required',
+                'text_title.min' => 'The title must have at least :min characters.',
+                'text_title.max' => 'The title must have a maximum of :max characters.',
+
+                'text_note.required' => 'Annotation is required',
+                'text_note.min' => 'The annotation must have at least :min characters.',
+                'text_note.max' => 'The annotation must have a maximum of :max characters.',
+            ]
+        );
+
+        // get user id
+        $id = session('user.id');
+
+        // create new note
+        $note = new Note();
+        $note->user_id = $id;
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        // redirect to home
+        return redirect()->route('home');
     }
 
     public function editNote($id) 
