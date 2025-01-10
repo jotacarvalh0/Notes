b32c3058a7b55653e31c8cd14f074b9a9bd335cd
jotacarvalh0 <josecarvalho11235@gmail.com>
2025-01-10 09:41:18 -0300

    FEAT: Editing notes.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -65,7 +65,13 @@ class MainController extends Controller
     public function editNote($id) 
     {
         $id = Operations::decryptId($id);
-        echo "I'm editing note with id = $id";
+        
+
+        // load note 
+        $note = Note::find($id);
+
+        // show edit note view
+        return view('edit_note', ['note' => $note]);
 
     }
     public function deleteNote($id) 
