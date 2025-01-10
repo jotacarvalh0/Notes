bba3001a36f809116f303b05f6528cc476e44187
jotacarvalh0 <josecarvalho11235@gmail.com>
2025-01-10 10:00:00 -0300

    REFACTOR: Implementing validation and applying the grade editing function.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -72,8 +72,49 @@ class MainController extends Controller
 
         // show edit note view
         return view('edit_note', ['note' => $note]);
-
     }
+
+    public function editNoteSubmit(request $request)
+    {
+        // validate request
+        $request->validate(
+            // rules
+            [
+                'text_title' => 'required | min:3 | max:200',
+                'text_note' => 'required | min:3 | max:3000'
+            ],
+            //error messages
+            [
+                'text_title.required' => 'Title is required',
+                'text_title.min' => 'The title must have at least :min characters.',
+                'text_title.max' => 'The title must have a maximum of :max characters.',
+
+                'text_note.required' => 'Annotation is required',
+                'text_note.min' => 'The annotation must have at least :min characters.',
+                'text_note.max' => 'The annotation must have a maximum of :max characters.',
+            ]
+        );
+
+        // chack if note_id exists
+        if($request->note_id == null) {
+            return redirect()->to('home');
+        }
+
+        // decrypt note_id
+        $id = Operations::decryptId($request->note_id);
+
+        // load note
+        $note = Note::find($id);
+
+        // update note
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        // redirect to home
+        return redirect()->route('home');
+    }
+
     public function deleteNote($id) 
     {
         $id = Operations::decryptId($id);
